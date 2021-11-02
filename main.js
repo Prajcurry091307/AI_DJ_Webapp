1cbbea1c262102efd8b9b87bd158538c29e1a241
Prajcurry091307 <77421995+Prajcurry091307@users.noreply.github.com>
2021-11-01 17:55:52 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 var song="";
 
 function preload()
@@ -12,8 +17,30 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded()
+{
+    console.log("PoseNet is initiallized");
 }
 
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.leftWristY;
+        console.log("leftWristX = "+leftWristX +" leftWristY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+rightWristX +" rightWristY = "+ rightWristY);
+
+    }
+}
 function draw()
 {
     image(video,0,0,600,500);
@@ -22,4 +49,6 @@ function draw()
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
